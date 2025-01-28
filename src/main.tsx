0e76754e403914309d2146533c96925c909fd069
Giacomo Pirani <giacomopirani1996@gmail.com>
2025-01-28 16:23:43 +0100

    fix: revert import app

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./app.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import "./index.css";
 
